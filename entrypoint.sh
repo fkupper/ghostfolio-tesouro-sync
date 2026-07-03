b5869396a177a522591664bde77abf64248d6a63
Fernando Küpper Cardoso <fernando@kupper.com.br>
2026-07-03 12:15:59 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
 # Extrai as variáveis de ambiente do Docker e as salva para o cron ler
 printenv | grep -v "no_proxy" > /etc/environment
 
 echo "================================================="
-echo "🚀 Iniciando o sincronizador Tesouro -> Ghostfolio"
-echo "📅 Cronjob configurado para rodar de Seg a Sex."
+echo "🚀 Iniciando o container Tesouro -> Ghostfolio"
 echo "================================================="
 
-# Inicia o cron em primeiro plano (foreground) para o Docker não desligar
+echo "▶️ Iniciando primeira execução imediata..."
+python /app/main.py
+
+echo "================================================="
+echo "📅 Primeira execução concluída."
+echo "⏳ Iniciando o agendador (cron) para rodar de Seg a Sex."
+echo "================================================="
+
 exec cron -f
